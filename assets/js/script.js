e017da85f94f12dcdd2d61a8c84d19e932f3ab08
JeffersonAguiar <jfdruida@gmail.com>
2024-01-08 20:41:14 +0000

    Feat: Add geolocation functionality to display user's latitude and longitude

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+function showPosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    document.getElementById("location").innerHTML = "Latitude: " + latitude + "<br>Longitude: " + longitude;
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+getLocation();
